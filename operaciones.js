90b5991ff00b351d4dc7a7ead08a97dbdab5aa19
Adrián Torres <binarytreecode@gmail.com>
2022-04-20 10:17:28 -0500

    termine razones trigonometricas

--- operaciones.js
+++ operaciones.js
@@ -2,7 +2,6 @@ var selec_pitagoras = document.getElementById("pitagoras_selec");
 const hipotenusa = document.getElementById("hipotenusa");
 const a_id = document.getElementById("cateto_a");
 const b_id = document.getElementById("cateto_b");
-var a, b, h, r;
 
 function modulo(type) {
     a = parseFloat(a_id.value);
@@ -33,7 +32,8 @@ function modulo(type) {
 
 
 var selec_razones_sacar = document.getElementById("razones_selec-sacar");
-var selec_razones_tiene = document.getElementById("razones_selec-tiene");
+var selec_razones_sacarLado = document.getElementById("razones_selec-sacar_lado");
+
 var lado = document.getElementById("lado");
 var angulo = document.getElementById("angulo");
 
@@ -44,109 +44,221 @@ Math.grados = function (radianes) {
     return radianes * 180 / Math.PI;
 };
 
+var check_h = document.getElementsByClassName("check_h");
+var check_a = document.getElementsByClassName("check_a");
+var check_b = document.getElementsByClassName("check_b");
 
 function modulo_razones() {
     type1 = selec_razones_sacar.value;
-    type2 = selec_razones_tiene.value;
+    type2 = selec_razones_sacarLado.value;;
+
+    h = {
+        value: parseInt(check_h[1].value),
+        buleano: check_h[0].checked,
+    }
+
+    a = {
+        value: parseInt(check_a[1].value),
+        buleano: check_a[0].checked,
+    }
+
+    b = {
+        value: parseInt(check_b[1].value),
+        buleano: check_b[0].checked,
+    }
+
 
     lado_value = parseFloat(lado.value);
     angulo_value = parseFloat(angulo.value);
     angulo_value = Math.radianes(angulo_value);
 
-    if (type1 == "hipotenusa") {
-        if (type2 == "b") {
-            h = lado_value / Math.sin(angulo_value);
-            input2 = b_id;
+    if (type1 == "lado") {
+        if (type2 == "hipotenusa") {
+            if (b.buleano == true) {
+                h = b.value / Math.sin(angulo_value);
+                // input2 = b_id;
+            }
+            else {//si tiene cateto abyacente
+                h = a.value / Math.cos(angulo_value);
+                // input2 = a_id;
+            }
+            r = h;
+            input = hipotenusa;
         }
-        else {//si tiene cateto abyacente
-            h = lado_value / Math.cos(angulo_value);
-            input2 = a_id;
+        else if (type2 == "cateto opuesto") {
+            if (h.buleano == true) {
+                b = h.value * Math.sin(angulo_value);
+                // input2 = hipotenusa;
+            }
+            else {
+                b = a.value * Math.tan(angulo_value);
+                // input2 = a_id;
+            }
+            r = b; //b = cateto opuesto
+            input = b_id;
         }
-        r = h;
-        input = hipotenusa;
-    }
-    else if (type1 == "cateto opuesto") {
-        if (type2 == "h") {
-            b = lado_value * Math.sin(angulo_value);
-            input2 = hipotenusa;
+        else if (type2 == "cateto adyacente") {
+            if (h.buleano == true) {
+                a = h.value * Math.cos(angulo_value);
+                // input2 = hipotenusa;
+            }
+            else { //si tiene b
+                a = b.value * Math.tan(angulo_value);
+                // input2 = b_id;
+            }
+            r = a; //a = cateto adyacente
+            input = a_id;
+        }
+        // input.value = r.toFixed(2);
+        // input2.value = lado_value.toFixed(2);
+
+        const content = document.getElementById('resultado');
+        content.innerHTML = `${type2}= ${r.toFixed(2)}`;
+
+        if (type2 == "hipotenusa") {
+            type2 = "cateto";
         }
         else {
-            b = lado_value * Math.tan(angulo_value);
-            input2 = a_id;
+            type2 = "hipotenusa";
         }
-        r = b; //b = cateto opuesto
-        input = b_id;
+        modulo(type2);
     }
-    else if (type1 == "cateto adyacente") {
-        if (type2 == "h") {
-            a = lado_value * Math.cos(angulo_value);
-            input2 = hipotenusa;
+    else {
+        if (h.buleano == true) {
+            if (a.buleano == true) {
+                Preangulo = a.value / h.value;
+                angulo = Math.grados(Math.asin(Preangulo));
+            }
+            else { //b
+                Preangulo = b.value / h.value;
+                angulo = Math.grados(Math.acos(Preangulo));
+            }
         }
-        else { //si tiene b
-            a = lado_value * Math.tan(angulo_value);
-            input2 = b_id;
+        else { //a && b
+            Preangulo = b.value / a.value;
+            angulo = Math.grados(Math.atan(Preangulo));
         }
-        r = a; //a = cateto adyacente
-        input = a_id;
-    }
-    else { }
 
-    input.value = r.toFixed(2);
-    input2.value = lado_value.toFixed(2);
-
-    const content = document.getElementById('resultado');
-    content.innerHTML = `${type1}= ${r.toFixed(2)}`;
-
-    if (type1 == "hipotenusa") {
-        type1 = "cateto";
-    }
-    else  {
-        type1 = "hipotenusa";
-    }
-    modulo(type1);
-    // a = {
-    //     b: a = lado / Math.sin(angulo),
-    //     c: a = lado / Math.cos(angulo)
-    // }
-    // b = {
-    //     a: b = lado * Math.sin(angulo),
-    //     c: b = lado * Math.tan(angulo)
-    // }
-    // c = {
-    //     a: c = lado * Math.cos(angulo),
-    //     b: c = lado * Math.tan(angulo)
-    // }
-}
-
-
-function razones2() {
-    if (h_modulo == true) {
-        if (a_modulo == true) {
-            Preangulo = a/h;
-            angulo = Math.ceil(Math.grados(Math.asin(Preangulo)));
-        }
-        else { //b
-            Preangulo = b/h;
-            angulo = Math.ceil(Math.grados(Math.acos(Preangulo)));
-        }
-    }
-    else { //a && b
-        Preangulo = b/a;
-        angulo = Math.ceil(Math.grados(Math.atan(Preangulo)));
+        const content = document.getElementById('resultado');
+        content.innerHTML = `angulo = ${angulo.toFixed(3)}`;
     }
 }
 
 
-function ley_senos () {
+const selector_razon = document.getElementById("razones_selec-sacar");
+const selector_razon__angulo = document.getElementById("razones_selec-tiene__angulo");
+const selector_lado = document.getElementById("lado");
+
+selector_razon.addEventListener("change", visible, false);
+
+
+function visible() {
+    v = selector_razon.value;
+    if (v == "angulo") {
+        selector_lado.style.display = "none";
+        selector_razon__angulo.style.display = "none";
+    }
+    else {
+        selector_lado.style.display = "block";
+        selector_razon__angulo.style.display = "block";
+    }
+}
+
+// const a = document.getElementById("a");
+// const b = document.getElementById("b");
+// const c = document.getElementById("c");
+
+// const A = document.getElementById("A");
+// const B = document.getElementById("B");
+// const C = document.getElementById("C");
+
+// recto = false;
+// function lector() {
+//     a_value = { value: a.value, nombre: "cateto adyacente" };
+//     b_value = { value: b.value, nombre: "cateto opuesto" };
+//     c_value = { value: c.value, nombre: "hipotenusa" };
+
+//     A_value = { value: check_a.value, nombre: "angulo A" };
+//     B_value = { value: check_b.value, nombre: "angulo B" };
+//     C_value = { value: C.value, nombre: "angulo C" };
+
+//     lados = [a_value, b_value, c_value];
+//     angulos = [A_value, B_value, C_value];
+//     angulos_radianes = Map(angulos, Math.radianes);
+
+//     var sum, angulo, sum2, C_angulos;
+//     angulos.forEach(element => {
+//         if (element.value == 90) {
+//             agudo = true;
+//             sum += element.value;
+//         }
+//         if (sum < 180) {
+//             angulo = 180 - sum;
+//         }
+//         if (element.value == '') {
+//             sum2++;
+//         }
+//         if (sum2 == 1) {
+//             if (element.value == '') {
+//                 element.value = angulo;;
+//             }
+//         }
+//         if (element.value != '' && element.value != 0) {
+//             C_angulos++;
+//         }
+//     });
+
+//     var C_lados = lados.reduce(element => {
+//         if (element != '' && element != 0) {
+//             C_lados++;
+//         }
+//     });
+// }
+
+// if (recto == true) {
+//     if (C_lados == 2) {
+//         if (lados[2].value != '' && lados[2].value != 0) {
+
+//         }
+//     }
+//     else {
+
+//     }
+// }
+
+
+function ley_senos(anguloA, ladoA, angulo) {
     if (lado == true) {
-        (a.lado* Math.sin(Math.radianes(angulo))) 
-        / Math.sin(Math.radianes(a.angulo));
+        x = ((ladoA * Math.sin(Math.radianes(angulo)))
+            / Math.sin(Math.radianes(anguloA)));
+    } else {
+        x = (Math.asin(
+            (lado * Math.sin(anguloA)) / ladoA
+        ));
+        Math.grados(angulo);
+    }
+    return x
+}
+
+function ley_senos() {
+    if (lado == true) {
+        (a.lado * Math.sin(Math.radianes(angulo)))
+            / Math.sin(Math.radianes(a.angulo));
     } else {
         Math.asin(
             (lado * Math.sin(a.angulo)) / a.lado
-            );
+        );
         Math.grados(a);
     }
 }
 
+function ley_cosenos(b, c, angulo) {
+    Math.sqrt((Math.pow(b, 2) + Math.pow(c, 2)) - 2 * b * c * Math.cos(Math.radianes(angulo)))
+}
+
+function ley_cosenos_modulo() {
+    if (a == true) {
+        ley_cosenos(b, c, angulo)
+    }
+}
 
